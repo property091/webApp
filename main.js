da678e55352f33a26abbf2f810231092f27b83e7
property091 <127836193+property091@users.noreply.github.com>
2023-07-02 17:25:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,35 +9,43 @@ function hide(x) {
     if(x == 1) {
         document.getElementById("tarifs").style.opacity = "0%";
         document.getElementById("tarifs").style.visibility = "hidden";
+        document.getElementById("tarifs").style.zIndex = "-1";
 
         document.getElementById("add").style.opacity = "0%";
         document.getElementById("add").style.visibility = "hidden";
+        document.getElementById("add").style.zIndex = "-1";
 
         document.getElementById("home").style.opacity = "100%";
         document.getElementById("home").style.visibility = "visible";
+        document.getElementById("home").style.zIndex = "2";
     }
 
     if (x == 2) {
         document.getElementById("tarifs").style.opacity = "100%";
         document.getElementById("tarifs").style.visibility = "visible";
-
+        document.getElementById("tarifs").style.zIndex = "2";
 
         document.getElementById("add").style.opacity = "0%";
         document.getElementById("add").style.visibility = "hidden";
+        document.getElementById("add").style.zIndex = "-1";
 
         document.getElementById("home").style.opacity = "0%";
         document.getElementById("home").style.visibility = "hidden";
+        document.getElementById("home").style.zIndex = "-1";
     }
     
     if (x == 3) {
         document.getElementById("tarifs").style.opacity = "0%";
         document.getElementById("tarifs").style.visibility = "hidden";
+        document.getElementById("tarifs").style.zIndex = "-1";
 
         document.getElementById("add").style.opacity = "100%";
         document.getElementById("add").style.visibility = "visible";
+        document.getElementById("add").style.zIndex = "2";
 
         document.getElementById("home").style.opacity = "0%";
         document.getElementById("home").style.visibility = "hidden";
+        document.getElementById("home").style.zIndex = "-1";
 
     }
 }
